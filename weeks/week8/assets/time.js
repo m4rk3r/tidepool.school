ecdd1fb84326c99417cd21a9c4686a9e25e27187
Mark Beasley <mbeasl1@gmail.com>
2021-03-10 13:14:27 -0500

    Week 8

---
+++ weeks/week8/assets/time.js
@@ -0,0 +1,21 @@
+const output = document.querySelector('.est-time');
+
+output.innerText = moment().tz("America/New_York").format("h:mm:ss a");
+
+setInterval(() => {
+  output.innerText = moment().tz("America/New_York").format("h:mm:ss a");
+}, 1000);
+
+
+const trs = document.querySelectorAll('tr');
+
+const start = moment('2021-03-12 09:00:00');
+const meetingLength = 7;
+
+trs.forEach((tr) => {
+  const td = tr.querySelector('td:last-child');
+  if (td) {
+    td.innerText = start.format("h:mm a");
+    start.add(meetingLength, 'minutes');
+  }
+})
